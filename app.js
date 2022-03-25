5637d09e7b3bdc23d9c4055476bdc0c8cce4fb43
alejandro Coral <alejandrocorald@gmail.com>
2022-03-24 19:32:48 -0500

    4

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT || 4001;
-
+const PORT = process.env.PORT || 5001;
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/views/home.html"));
 });
 app.get("/register", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/views/register.html"));
 });
-app.listen(PORT, () => console.log("corriendo port 4001"));
+app.listen(PORT, () => console.log("corriendo port 5001"));
